ed1363b346b0eb180bb0cec7272cb7b6c7133a52
Jayesh Rathi <itsjayeshrathi@gmail.com>
2024-06-14 13:08:04 +0530

    add regsiter controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,7 +50,7 @@ const userSchema = new Schema(
 //! here don't use the arrow function since we will not have the context of this
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 userSchema.methods.isPasswordCorrect = async function (password) {
