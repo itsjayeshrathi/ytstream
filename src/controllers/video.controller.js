1d2b29c70266d4075cee5a1cbe767c31ba51b32d
Jayesh Rathi <69662354+itsjayeshrathi@users.noreply.github.com>
2024-06-16 14:42:10 +0530

    added video controller and router

---
+++ src/controllers/video.controller.js
@@ -0,0 +1,38 @@
+import { Video } from "../models/video.model";
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+
+const getAllVideos = asyncHandler(async (req, res) => {
+  const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+});
+
+const publishAVideo = asyncHandler(async (req, res) => {
+  const { title, description } = req.body;
+});
+const getVideoById = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+});
+const updateVideo = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+ 
+});
+const deleteVideo = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  const deleteVideo = await Video.findByIdAndDelete(videoId);
+  if (deleteVideo) {
+    return res.status(200).json(200, "user delete successfully");
+  } else {
+    throw new ApiError(404, "video not found");
+  }
+});
+const togglePublishStatus = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+});
+export {
+  getAllVideos,
+  publishAVideo,
+  getVideoById,
+  updateVideo,
+  deleteVideo,
+  togglePublishStatus,
+};
